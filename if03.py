1800950762a411280abdf6744e39f2b98285ed28
QAmirkhan1177 <amirxonqarshiyev@gmail.com>
2023-12-26 13:09:51 +0500

    3 tayyor

--- if03.py
+++ if03.py
@@ -8,4 +8,10 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a>b and b>c or c>b and b>a:
+        return b
+    if b>c and c>a or a>c and c>b:
+        return c
+    else:
+        return a
+print(main(3,7,4))
